f61b84e2047c959f427020cefed9ac495fbc7395
neil mehra <neilmehra@outlook.com>
2023-05-25 22:26:07 -0400

    populating

--- app/scrapers/populate.py
+++ app/scrapers/populate.py
@@ -174,7 +174,14 @@ class DramaParser:
         title, year = self.parse_title_year(soup)
         type = 'Drama'
         description = self.parse_description(soup)
-        rating = self.parse_rating(soup)
+        rating, ratings, watchers, reviews = self.parse_user_info(soup)
+        native_title = self.parse_native_title(soup)
+        known_as = self.parse_known_as(soup)
+        screenwriter = self.parse_screenwriter(soup)
+        director = self.parse_director(soup)
+        country, type, episodes, aired, aired_on, original_network, duration, ranked, popularity, content_rating = self.parse_infotable(soup)
+        genres = self.parse_genres(soup)
+        tags = self.parse_tags(soup)
 
     def parse_title_year(self, soup: BeautifulSoup) -> Tuple[str, str]:
         pattern = r"(.+) \((\d+)\) -"
@@ -191,9 +198,88 @@ class DramaParser:
         except:
             return None
 
-    def parse_rating(self, soup: BeautifulSoup) -> float:
-        return float(soup.find('b', attrs={'itempropx':'ratingValue'}).text)
+    def parse_user_info(self, soup: BeautifulSoup) -> Tuple:
+        elements = soup.find_all('div', attrs={'class':'hfs'})
+        all_text = ""
+        for element in elements: all_text += element.text.strip()
+        pattern = r".*: (.*)\/.* from (.*) users.*: (.*)Reviews: (.*) users"
+        p = re.compile(pattern)
+        m = re.search(p, all_text)
 
-    def parse_num_ratings(self, soup: BeautifulSoup) -> int:
-        text = soup.find('div', attrs={'class':'hfs', 'itempropx':'aggregateRating'})
+        def clean_str(s): return s.replace(',', '')
+
+        return float(m.group(1)), int(clean_str(m.group(2))), int(clean_str(m.group(3))), int(clean_str(m.group(4)))
+
+    def __element_parser(self, elements: List[BeautifulSoup], query: str, colon: bool = True) -> Optional[str]:
+        for element in elements:
+            if query in element.text:
+                text = element.text.strip()
+                if colon:
+                    return text[text.find(':')+2:]
+                return text
+        return None
+
+    def parse_native_title(self, soup: BeautifulSoup) -> Optional[str]:
+        try:
+            elements = soup.find_all('li', attrs={'class':'list-item p-a-0'})
+            return self.__element_parser(elements, "Native Title")
+        except:
+            return None
+
+    def parse_known_as(self, soup: BeautifulSoup) -> Optional[List[str]]:
+        try:
+            aka_titles = soup.find('span', attrs={'class':'mdl-aka-titles'})
+            children = aka_titles.findChildren('a', recursive=False)
+            return list(map(lambda x: x.text, children))
+        except:
+            return None
+
+    def parse_screenwriter(self, soup: BeautifulSoup) -> Optional[str]:
+        try:
+            elements = soup.find_all('li', attrs={'class':'list-item p-a-0'})
+            return self.__element_parser(elements, "Screenwriter")
+        except:
+            return None
+
+    def parse_director(self, soup: BeautifulSoup) -> Optional[str]:
+        try:
+            elements = soup.find_all('li', attrs={'class':'list-item p-a-0'})
+            return self.__element_parser(elements, "Director")
+        except:
+            return None
+
+    def parse_infotable(self, soup: BeautifulSoup) -> Tuple:
+        infotable = soup.find('ul', attrs={'class':'list m-a-0 hidden-md-up'})
+        infotable_elements = infotable.find_all('li', attrs={'class':'list-item p-a-0'})
+
+        country = self.__element_parser(infotable_elements, 'Country')
+        type = self.__element_parser(infotable_elements, 'Drama')
+        episodes = self.__element_parser(infotable_elements, 'Episodes')
+        aired = self.__element_parser(infotable_elements, 'Aired')
+        aired_on = self.__element_parser(infotable_elements, 'Aired On')
+        original_network = self.__element_parser(infotable_elements, 'Original Network')
+        duration = self.__element_parser(infotable_elements, 'Duration')
+        ranked = self.__element_parser(infotable_elements, 'Ranked')
+        popularity = self.__element_parser(infotable_elements, 'Popularity')
+        content_rating = self.__element_parser(infotable_elements, 'Content Rating')
+
+        return country, type, episodes, aired, aired_on, original_network, duration, ranked, popularity, content_rating
+
+    def parse_genres(self, soup: BeautifulSoup) -> Optional[List[str]]:
+        try:
+            genres = soup.find('li', attrs={'class':'list-item p-a-0 show-genres'}).find_all('a')
+            return list(map(lambda x: x.text.strip(), genres))
+        except:
+            return None
+
+
+    def parse_tags(self, soup: BeautifulSoup) -> Optional[List[str]]:
+        try:
+            tags = soup.find('li', attrs={'class':'list-item p-a-0 show-tags'}).find_all('a', attrs={'class':'text-primary'})
+            return list(map(lambda x: x.text.strip(), tags))
+        except:
+            return None
+
+
+
 
